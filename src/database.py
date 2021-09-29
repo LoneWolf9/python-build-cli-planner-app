4fbe79957115df6498a20798ead3487786de1ff2
LoneWolf9 <lstdon9@yahoo.com>
2021-09-29 09:32:09 -0600

    Update database.py

--- src/database.py
+++ src/database.py
@@ -16,6 +16,7 @@ def list_reminders():
 
 
 def add_reminder(text, date, ReminderClass):
+    issubclass(ReminderClass, DeadlinedReminder)
     if not issubclass(ReminderClass, DeadlinedReminder):
         TypeError('Invalid Reminder Class')
     reminder = ReminderClass(text, date)
